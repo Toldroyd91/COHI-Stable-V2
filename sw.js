d7fea6890099cb660680fa45839710ce63294ce2
Toldroyd91 <thomas.oldroyd@hotmail.com>
2026-06-05 19:01:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = 'survey-app-v1';
+const CACHE_NAME = 'survey-app-v5-luxury';
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './logo.jpg',
-  './sap-pamphlet.jpg',
-  './planning-pamphlet.jpg',
-  './cavity-pamphlet.jpg'
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './designers.js',
+    './logo.jpg'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
 });
